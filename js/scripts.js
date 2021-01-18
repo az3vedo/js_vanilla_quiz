9fc2e95840e383f40a99af01bed6c959311dc1af
Gabriel Souza <azevedoedit@gmail.com>
2021-01-18 02:20:31 -0300

    add logic

---
+++ js/scripts.js
@@ -0,0 +1,194 @@
+const question       = document.querySelector("#question");
+const answersBox     = document.querySelector("#answers-box");
+const quizCotainer   = document.querySelector("#quiz-container");
+const scoreContainer = document.querySelector("#score-container");
+const letters        = ["a", "b", "c", "d"];
+let points = 0;
+let actualQuestion = 0;
+
+// Perguntas
+const questions = [
+  {
+    "question": "PHP foi desenvolvido para qual fim?",
+    "answers": [
+      {
+        "answer": "back-end",
+        "correct": true
+      },
+      {
+        "answer": "front-end",
+        "correct": false
+      },
+      {
+        "answer": "Sistema operacional",
+        "correct": false
+      },
+      {
+        "answer": "Banco de dados",
+        "correct": false
+      },
+    ]
+  },
+  {
+    "question": "Uma forma de declarar variável em JavaScript:",
+    "answers": [
+      {
+        "answer": "$var",
+        "correct": false
+      },
+      {
+        "answer": "var",
+        "correct": true
+      },
+      {
+        "answer": "@var",
+        "correct": false
+      },
+      {
+        "answer": "#let",
+        "correct": false
+      },
+    ]
+  },
+  {
+    "question": "Qual o seletor de id no CSS?",
+    "answers": [
+      {
+        "answer": "#",
+        "correct": true
+      },
+      {
+        "answer": ".",
+        "correct": false
+      },
+      {
+        "answer": "@",
+        "correct": false
+      },
+      {
+        "answer": "/",
+        "correct": false
+      },
+    ]
+  },
+]
+
+// Substituição do quiz para a primeira pergunta
+function init() {
+  createQuestion(0);
+}
+
+// Cria uma pergunta
+function createQuestion(i) {
+
+  // Limpar a questão anterior
+  const oldButtons = answersBox.querySelectorAll("button")
+  
+  oldButtons.forEach(function(btn){
+    btn.remove( )
+  })
+
+  // Altera o texto da pergunta
+  const questionText    = question.querySelector("#question-text");
+  const questionNumber  = question.querySelector("#question-counter");
+
+  questionText.textContent   = questions[i].question;
+  questionNumber.textContent = i+1;
+
+  // Insere as alternativas
+  questions[i].answers.forEach(function(answer,i) {
+
+    // Cria o template do botão do quiz
+    const answerTemplate = document.querySelector(".answer-template").cloneNode(true);
+    const letterBtn = answerTemplate.querySelector(".btn-letter");
+    const answerText = answerTemplate.querySelector(".question-answer")
+
+    letterBtn.textContent = letters[i];
+    answerText.textContent = answer['answer'];
+
+    answerTemplate.setAttribute("correct-answer", answer["correct"]);
+
+    // Remover hide e template class
+    answerTemplate.classList.remove("hide");
+    answerTemplate.classList.remove("answer-template");
+
+    // Inserir a alternativa na tela 
+
+    answersBox.appendChild(answerTemplate);
+
+    // Inserir um evento de click no botão
+    answerTemplate.addEventListener("click", function() {
+      checkAnswer(this);
+    });
+  });
+
+  // Incrementar o número da questão
+  actualQuestion++;
+
+}
+
+// Verificando resposta do usuário
+function checkAnswer(btn) {
+
+  const buttons = answersBox.querySelectorAll("button");
+  
+  buttons.forEach(function(button) {
+    if(button.getAttribute("correct-answer") === "true") {
+        button.classList.add("correct-answer");
+        if(btn === button){
+          points++;
+        }
+      } else {
+        button.classList.add("wrong-answer");
+      }
+
+  });
+  nextQuestion();
+}
+
+// Exibe a próxima pergunta
+function nextQuestion(){
+
+  setTimeout(function(){
+    if (actualQuestion >= questions.length) {
+      showSuccessMessage();
+      return;
+    }
+  
+    createQuestion(actualQuestion);
+  }, 700)
+
+}
+
+// Exibe a mensagem de sucesso
+function showSuccessMessage(){
+  hideOrShowQuiz();
+
+  const score = (points / questions.length * 100).toFixed(2);
+  const displayScore = document.querySelector("#display-score span");
+
+  displayScore.textContent = score.toString();
+
+  const correctAnswers = document.querySelector("#correct-answers");
+  correctAnswers.textContent = points;
+
+  const questionsQty = document.querySelector("#questions-qty")
+  questionsQty.textContent = questions.length;
+  
+}
+
+function hideOrShowQuiz() {
+  quizCotainer.classList.toggle("hide");
+  scoreContainer.classList.toggle("hide");
+}
+
+const restartBtn = document.querySelector("#restart");
+restartBtn.addEventListener("click", function() {
+  actualQuestion = 0;
+  points = 0;  
+  hideOrShowQuiz();
+  init();
+})
+
+// Inicialização do Quiz
+init();
